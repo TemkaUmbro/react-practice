dc8f3c24d58bfeda791be0254ce31d2172bf070b
Artsiom Umbryian <trueweightlifter@gmail.com>
2021-08-30 18:00:16 +0300

    add 1st project

---
+++ 1st-project/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './App.module.scss';
+
+const App = () => {
+  return (
+    <div className={s.wrapper}>
+      hi
+    </div>
+  )
+}
+
+export default App;
